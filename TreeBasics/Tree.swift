c973c2a3cb141cab8e53076cfcd9adf417e7721d
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-03-18 20:15:15 +0900

    before test

---
+++ TreeBasics/Tree.swift
@@ -0,0 +1,115 @@
+//
+//  Tree.swift
+//  TreeBasics
+//
+//  Created by Yuki Tsukada on 2021/03/18.
+//
+
+import Foundation
+
+// Who’s my parent?
+func findParent() {
+    print("Please input tree for findParent")
+    var parents = [Int:Int]()
+    var alreadyExist = [Int:Bool]()
+    let n = Int(readLine()!)!
+    alreadyExist[1] = true
+    for i in 2...n {
+        alreadyExist[i] = false
+    }
+    
+    for _ in 0..<n-1 {
+        let nodeInfo = readLine()!.split(separator: " ").map { Int($0) }
+        if alreadyExist[nodeInfo[0]!] == false {
+            parents[nodeInfo[0]!] = nodeInfo[1]
+            alreadyExist[nodeInfo[0]!] = true
+        }
+        if alreadyExist[nodeInfo[1]!] == false {
+            parents[nodeInfo[1]!] = nodeInfo[0]
+            alreadyExist[nodeInfo[1]!] = true
+        }
+    }
+    print("Output")
+    for i in 2...n {
+        print(parents[i]!)
+    }
+}
+
+
+// Diameter
+var alreadyPassed = [Int:Bool]()
+var tempDistance: Int = 0
+var longestDistanceValue: Int = 0
+
+func longestDistance() {
+    print("Please input tree for longestDistance")
+    var distanceArray: [Dictionary<Int, Int>] = []
+    var distance = [Int:Int]()
+    var paths = [Int:[Dictionary<Int, Int>]]()
+    
+    let n = Int(readLine()!)!
+    for i in 1...n {
+        alreadyPassed[i] = false
+    }
+    for _ in 0..<n {
+        let nodeInfo = readLine()!.split(separator: " ").map { Int($0) }
+        var oddIndex = 1
+        var evenIndex = 2
+        while nodeInfo[oddIndex] != -1 {
+            distance[nodeInfo[oddIndex]!] = nodeInfo[evenIndex]
+            distanceArray.append(distance)
+            distance.removeAll()
+            oddIndex += 2
+            evenIndex += 2
+        }
+        paths[nodeInfo[0]!] = distanceArray
+        distanceArray.removeAll()
+    }
+    
+    for i in 1...n {
+        longestDistanceHelper(startPoint: i, paths: paths)
+        tempDistance = 0
+        for i in 1...n {
+            alreadyPassed[i] = false
+        }
+    }
+    print("Output \(longestDistanceValue)")
+}
+
+
+func longestDistanceHelper(startPoint: Int, paths: [Int:[Dictionary<Int, Int>]]) {
+    alreadyPassed[startPoint] = true
+    var nextPoints = [Int]()
+    var distanceOfCurrentPath = 0
+    for eachDic in paths[startPoint]! {
+        nextPoints.append(eachDic.keys.first!)
+    }
+    if isEndPoint(intArray: nextPoints) {
+        if tempDistance > longestDistanceValue {
+            longestDistanceValue = tempDistance
+        }
+    }
+    
+    // iterate possible nodes from one node
+    for eachDic in paths[startPoint]! {
+        if !alreadyPassed[eachDic.keys.first!]! {
+            // the distance of the current choice
+            distanceOfCurrentPath = eachDic.values.first!
+            tempDistance += distanceOfCurrentPath
+            longestDistanceHelper(startPoint: eachDic.keys.first!, paths: paths)
+            // before proceed to other choces, subtract the distance of the current choice
+            tempDistance -= distanceOfCurrentPath
+            
+        }
+    }
+    
+}
+
+func isEndPoint(intArray: [Int]) -> Bool {
+    for i in intArray {
+        if alreadyPassed[i] == false {
+            return false
+        }
+    }
+    return true
+}
---
+++ TreeBasics/main.swift
@@ -0,0 +1,103 @@
+//
+//  main.swift
+//  TreeBasics
+//
+//  Created by Yuki Tsukada on 2021/03/18.
+//
+
+import Foundation
+
+//findParent()
+/*
+ Sample Input1
+ 7
+ 1 6
+ 6 3
+ 3 5
+ 4 1
+ 2 4
+ 4 7
+ 
+ Sample Output1
+ 4
+ 6
+ 1
+ 3
+ 1
+ 4
+ 
+ Sample Input2
+ 12
+ 1 2
+ 1 3
+ 2 4
+ 3 5
+ 3 6
+ 4 7
+ 4 8
+ 5 9
+ 5 10
+ 6 11
+ 6 12
+
+ Sample Output2
+ 1
+ 1
+ 2
+ 3
+ 3
+ 4
+ 4
+ 5
+ 5
+ 6
+ 6
+
+ Sample Input3
+ 8
+ 1 8
+ 3 1
+ 2 3
+ 5 3
+ 4 8
+ 8 6
+ 6 7
+ 
+ Sample Output3
+ 3
+ 1
+ 8
+ 3
+ 8
+ 6
+ 1
+ */
+
+longestDistance()
+
+/*
+ Sample Input1
+ 5
+ 1 3 2 -1
+ 2 4 4 -1
+ 3 1 2 4 3 -1
+ 4 2 4 3 3 5 6 -1
+ 5 4 6 -1
+
+ Sample Output1
+ 11
+
+ Sample Input2
+ 8
+ 1 3 2 -1
+ 2 4 4 -1
+ 3 1 2 4 3 6 2 -1
+ 4 2 4 3 3 5 6 -1
+ 5 4 6 -1
+ 6 3 2 7 4 -1
+ 7 6 4 8 5 -1
+ 8 7 5 -1
+
+ Sample Output2
+ 20
+ */
